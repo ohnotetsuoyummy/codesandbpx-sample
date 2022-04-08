8d93821d18f314b7a6e14a15e6fb3ac65e8bfde8
オオノテツオ <87118166+ohnotetsuoyummy@users.noreply.github.com>
2022-04-08 14:06:37 +0900

    aaa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import "./styles.css";
 
 const onClickedAdd = () => {
-  alert();
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+
+  //li生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //p作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //完了button作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //削除button作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(li)を未完了リストから削除
+    const deleteTargetDiv = deleteButton.parentNode;
+    const deleteTargetLi = deleteTargetDiv.parentNode;
+    console.log(deleteTargetLi);
+    document.getElementById("incomplete-list").removeChild(deleteTargetLi);
+  });
+
+  //divタグの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document
